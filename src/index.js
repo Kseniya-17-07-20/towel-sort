b027faef75531f7db350042411d4b2226b6c1b5a
Kseniya Ivanova <kseniya.i369@mail.ru>
2022-08-12 22:49:50 +0300

    add decision

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
 
-// You should implement your task here.
+function towelSort (matrix){
+  typeof matrix !== "undefined"
+  if(!matrix) {return []} 
+  else {  
+    for (let i=0; i<matrix.length; i++){
 
+    for( let j=0; j< matrix[i].length; j++){
+       let index = i%2===0
+       ? j
+       : (matrix[i].length -j -1);
+
+       console.log(matrix[i][index])
+    }
+  }
+  }
+}
 module.exports = function towelSort (matrix) {
   return [];
 }
